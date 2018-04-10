abaef7ce0dacd2e30e78b3b5d060eac464ce01b4
Andre Pires <andre.pires@appdirect.com>
2018-04-09 23:20:29 -0400

    starting the app locally to allow pact to verify

--- provider-service/src/main/java/com/appdirect/pact/provider/ProductsApiController.java
+++ provider-service/src/main/java/com/appdirect/pact/provider/ProductsApiController.java
@@ -1,8 +1,13 @@
 package com.appdirect.pact.provider;
 
+import static org.springframework.http.HttpStatus.OK;
+
 import java.util.Arrays;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +17,38 @@ import com.appdirect.provider.model.ProductWsDTO;
 @RestController
 public class ProductsApiController implements ProductsApi {
 
+	private static final String PACT_REQUEST = "Pact-Request";
+	@Autowired
+	private HttpServletRequest request;
+
 	@Override
 	public ResponseEntity<List<ProductWsDTO>> getProducts() {
+		final String pactRequest = getRequest().getHeader(PACT_REQUEST);
+		if (pactRequest != null) {
+
+			return ResponseEntity.status(OK)
+					.header(PACT_REQUEST, PACT_REQUEST)
+					.body(
+						Arrays.asList(
+						new ProductWsDTO().id("id-1").name("name-1"),
+						new ProductWsDTO().id("id-2").name("name-2"),
+						new ProductWsDTO().id("id-3").name("name-3")
+					));
+
+		}
 		return ResponseEntity.ok(
 				Arrays.asList(
 						new ProductWsDTO().id("1").name("Sunscreen"),
 						new ProductWsDTO().id("2").name("Summer chair")
 				)
 		);
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
 	}
 }
